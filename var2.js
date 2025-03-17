baac9c2a8c402877a8b96fc0f4b850de14aeff43
Muhammad Arslan <135339315+Ar45slan56@users.noreply.github.com>
2025-03-17 19:38:06 +0000

    console.table

---
+++ var2.js
@@ -0,0 +1,13 @@
+const accountId = 12548
+let accountEmail = "arman@gmail.com"
+var password = "arman123"
+accountCity = "Lahore"
+
+// accountId = 2  //not allowed to change constant variable
+
+accountEmail = "arfam@gmail.com"
+password = "arfan123"
+accountCity = "Islamabad"
+
+// console .table returns automatically tabular form
+console.table([accountId,accountEmail,password, accountCity]);
